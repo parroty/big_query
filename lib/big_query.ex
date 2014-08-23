0338502ba1bc0c4450cbc4d379e38337ede81512
parroty <parroty00@gmail.com>
2014-08-23 10:35:08 +0900

    Temporary save

--- lib/big_query.ex
+++ lib/big_query.ex
@@ -1,5 +1,5 @@
 defmodule BigQuery do
-  defdelegate projects,                       to: BigQuery.API.Base
+  # defdelegate projects,                       to: BigQuery.API.Base
   defdelegate datasets(project),              to: BigQuery.API.Base
   defdelegate jobs(project),                  to: BigQuery.API.Base
   defdelegate query(project, query),          to: BigQuery.API.Base
@@ -19,17 +19,77 @@ defmodule BigQuery do
         authorize_url: OAuth2Ex.Site.Google.authorize_url,
         token_url:     OAuth2Ex.Site.Google.token_url,
         scope:         Enum.join(scopes, " "),
-        callback_url:  "http://localhost:4000"
+        callback_url:  "http://localhost:4000",
+        token_store:   System.user_home <> "/oauth2ex.google.token"
       )
-    end
-
-    def token_store_path do
-      System.user_home <> "/oauth2ex.google.token"
     end
   end
 
   def configure do
     {:ok, msg} = OAuth2Ex.Token.Manager.configure(OAuth2ExAdapter, port: 4000)
     IO.puts msg
+  end
+
+  def projects do
+    url = "https://www.googleapis.com/bigquery/v2/projects"
+    OAuth2Ex.Request.get(OAuth2ExAdapter, url).body
+  end
+end
+
+defmodule GitHub do
+  defmodule OAuth2ExAdapter do
+    use OAuth2Ex.Adapter
+
+    def config do
+      OAuth2Ex.config(
+        id:            System.get_env("GITHUB_API_CLIENT_ID"),
+        secret:        System.get_env("GITHUB_API_CLIENT_SECRET"),
+        authorize_url: OAuth2Ex.Site.GitHub.authorize_url,
+        token_url:     OAuth2Ex.Site.GitHub.token_url,
+        scope:         ["public_repo"],
+        callback_url:  "http://localhost:4000",
+        token_store:   System.user_home <> "/oauth2ex.github.token",
+        header_prefix: "token" # Authorization: token OAUTH-TOKEN
+      )
+    end
+  end
+
+  def configure do
+    {:ok, msg} = OAuth2Ex.Token.Manager.configure(OAuth2ExAdapter, port: 4000)
+    IO.puts msg
+  end
+
+  def get_authorization do
+    url = "https://api.github.com/authorizations"
+    OAuth2Ex.Request.get(OAuth2ExAdapter, url).body
+  end
+end
+
+defmodule Dropbox do
+  defmodule OAuth2ExAdapter do
+    use OAuth2Ex.Adapter
+
+    def config do
+      OAuth2Ex.config(
+        id:            System.get_env("DROPBOX_API_CLIENT_ID"),
+        secret:        System.get_env("DROPBOX_API_CLIENT_SECRET"),
+        authorize_url: OAuth2Ex.Site.Dropbox.authorize_url,
+        token_url:     OAuth2Ex.Site.Dropbox.token_url,
+        scope:         [],
+        callback_url:  "http://localhost:4000",
+        token_store:   System.user_home <> "/oauth2ex.dropbox.token",
+        header_prefix: "Bearer" # Authorization: Bearer <access token>
+      )
+    end
+  end
+
+  def configure do
+    {:ok, msg} = OAuth2Ex.Token.Manager.configure(OAuth2ExAdapter, port: 4000)
+    IO.puts msg
+  end
+
+  def get_account do
+    url = "https://api.dropbox.com/1/account/info"
+    OAuth2Ex.Request.get(OAuth2ExAdapter, url).body
   end
 end
